f01d918ea76a043743f278cb5f9aaf0290f0de55
urandu <bildadnamawa@gmail.com>
2018-01-18 09:25:50 +0300

    added authentication

--- django_tutorial/quickstart/views.py
+++ django_tutorial/quickstart/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from django_tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
@@ -9,7 +9,8 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited
      
     """
-    
+    permission_classes = (permissions.IsAuthenticated,)
+
     queryset = User.objects.all().order_by('date_joined')
     serializer_class = UserSerializer
     
@@ -20,6 +21,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows Groups to be viewed or edited
      
     """
-    
+    permission_classes = (permissions.IsAuthenticated,)
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
